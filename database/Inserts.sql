04d41aa81780be0061e22d6fa6739fed2beb7af1
Łukasz Starus <209464@edu.p.lodz.pl>
2019-04-11 17:04:26 +0200

    Database insterts

---
+++ database/Inserts.sql
@@ -0,0 +1,16 @@
+insert into Client values ('Heniu', 'Guziak', 'Kasztanowo 22', '99827188822');
+insert into InternetType values ('FastInternet');
+insert into InternetType values ('MediumInternet');
+insert into InternetType values ('SlowInternet');
+insert into Internet values (1);
+insert into Internet values (2);
+insert into Internet values (3);
+insert into FastInternet values (1, 10, 20)
+insert into MediumInternet values (2, 5, 10)
+insert into SlowInternet values (3, 1, 3)
+insert into Office values ('KasztanowaOffice', '£ód¿, Krzyczalskiego 22')
+insert into Services values ('InternetDlaDziadka+',3, 25);
+insert into Services values ('InternetDlaMamy+',2, 50);
+insert into Services values ('InternetDlasyna+',1, 99);
+insert into Workers values ('Bogdan', 'Byczek', 666,666555444, 'Ko³acin 22', '99827133221', '11-11-2010', 1)
+insert into Transactions values ('05-05-2019',99, 1, 1, 1)
