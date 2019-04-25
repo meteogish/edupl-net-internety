38a3d18cfdee2713ad119470c2df7a434d512a9c
Łukasz Starus <209464@edu.p.lodz.pl>
2019-04-25 15:38:52 +0200

    CreateTransaction Model

---
+++ src/Inter.Web/Database/Models/TransactionCreateData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inter.Web.Database.Models
+{
+    public class TransactionCreateData
+    {
+        public DateTime Date { get; set; }
+        public long ServiceId { get; set; }
+        public long ClientId { get; set; }
+        public long WorkerId { get; set; }
+
+        public string ServiceName { get; set; }
+        public string ClientName { get; set; }
+        public string ClientSurname { get; set; }
+        public string WorkerName { get; set; }
+        public string WorkerSurname { get; set; }
+        public double Price { get; set; }
+    }
+}
