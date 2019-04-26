918a874b4695663fef56e69854397917fe27fbfd
Łukasz Starus <209464@edu.p.lodz.pl>
2019-04-26 12:16:00 +0200

    AddTransactionn method added

--- src/Inter.Web/Database/Repositories/TransactionsRepository.cs
+++ src/Inter.Web/Database/Repositories/TransactionsRepository.cs
@@ -23,22 +23,22 @@ namespace Inter.Web.Database.Repositories
             var selector = builder.AddTemplate(
                 @"
                 select t.Transaction_ID as TransactionId, 
-t.Date, 
-o.Office_ID as OfficeId, 
-o.Name as OfficeName, 
-w.Worker_ID as WorkerId, 
-w.Name as WorkerName,
-w.Surname as WorkerSurname, 
-it.InternetType_ID as InternetTypeId, 
-it.InternetType_Name as InternetType, 
-t.Price 
-from Transactions t
-join Services s on t.Service_ID=s.Service_ID
-join Internet i on s.Internet_ID=i.Internet_ID
-join InternetType it on it.InternetType_ID=i.InternetType_ID
-join Workers w on w.Worker_ID=t.Worker_ID
-join Office o on o.Office_ID=w.Office_ID
-/**where**/
+                t.Date, 
+                o.Office_ID as OfficeId, 
+                o.Name as OfficeName, 
+                w.Worker_ID as WorkerId, 
+                w.Name as WorkerName,
+                w.Surname as WorkerSurname, 
+                it.InternetType_ID as InternetTypeId, 
+                it.InternetType_Name as InternetType, 
+                t.Price 
+                from Transactions t
+                join Services s on t.Service_ID=s.Service_ID
+                join Internet i on s.Internet_ID=i.Internet_ID
+                join InternetType it on it.InternetType_ID=i.InternetType_ID
+                join Workers w on w.Worker_ID=t.Worker_ID
+                join Office o on o.Office_ID=w.Office_ID
+                /**where**/
                 "
             );
             
@@ -66,9 +66,29 @@ join Office o on o.Office_ID=w.Office_ID
             {
                 builder.Where("w.Worker_ID in @WorkerIds");
             }
-            
-            var transInfo=conn.Query<TransactionInfoResult>(selector.RawSql, filter);
+
+            var transInfo = conn.Query<TransactionInfoResult>(selector.RawSql, filter);
             return transInfo;
+           
+        }
+
+        public void AddTransaction(TransactionCreateData createData)
+        {
+            string insert = @"Insert into transactions values(@Date, @Price, @Service_ID, @Client_ID, @Worker_ID)";
+
+            using (conn)
+            {
+                var transInfo = conn.Execute(insert,
+                new
+                {
+                    Date = createData.Date,
+                    Service_ID = createData.ServiceId,
+                    Client_ID = createData.ClientId,
+                    Worker_ID = createData.WorkerId,
+                    Price = (double)99.00
+                });
+
+            }
         }
     }
 }
